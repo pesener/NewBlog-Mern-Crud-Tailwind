78a6ccc435be92ba6139a0b5bc29f3b3d10ae091
hakan pesener <pesener@windowslive.com>
2023-01-08 14:56:22 +0300

    redux thunk ve devTools yüklendi.

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import postsReducer from "./reducers/postsReducer";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+  posts: postsReducer,
+});
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
