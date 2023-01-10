c0f23c5b8c46afafb3097a2926f5275832d36c9c
hakan pesener <pesener@windowslive.com>
2023-01-10 20:37:58 +0300

    sign up backend yazım işlemleri

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Footer from "./components/Footer";
-
+import { Toaster } from "react-hot-toast";
 import HomeScreen from "./screens/HomeScreen";
 import CreateScreen from "./screens/CreateScreen";
 import UpdateScreen from "./screens/UpdateScreen";
@@ -27,6 +27,12 @@ function App() {
         </div>
       </main>
       <Footer />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 2500,
+        }}
+      />
     </Router>
   );
 }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,22 +1,79 @@
-import React from 'react'
+import React,{useEffect,useState,useMemo
+} from 'react'
+import Select from 'react-select'
+import countryList from "react-select-country-list";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { register } from "../axios";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [countrySelect, setCountrySelect] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    surname: "",
+    countries: countrySelect,
+    email: "",
+    password: "",
+    correctionPassword: "",
+  });
+
+
+
+  const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    if (
+      formData.password.length >= 8 &&
+      formData.correctionPassword === formData.password
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [formData]);
+
+  const options = useMemo(() => countryList().getData(), []);
+
+  const changeHandler = (countrySelect) => {
+    setCountrySelect(countrySelect);
+  };
+
+  useEffect(() => {
+    setFormData({ ...formData, countries: countrySelect });
+  }, [countrySelect]);
+
+
   return (
     <div className='min-h-screen'>
     <div className='flex justify-center '>
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md sm:p-9   md:p-5 dark:bg-gray-700 dark:border-gray-800">
-      <form class="space-y-3" action="#">
+<div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md sm:p-9   md:p-5 dark:bg-gray-700 dark:border-gray-800">
+      <form className="space-y-3" action="#"
+         onSubmit={(e) => {
+          e.preventDefault();
+
+          register(formData)
+            .then((res) => {
+              navigate("/signin");
+            })
+            .catch((err) => {
+              toast.error(err.response.data.message);
+            });
+        }}
+      >
      
 
         <div>
           <label
             
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Name
           </label>
           <input 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
+          onChange={(e) =>
+            setFormData({ ...formData, name: e.target.value })
+          }
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm
             rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
             p-2.5 dark:bg-gray-600 dark:border-gray-500
             dark:placeholder-gray-400 dark:text-white" required>
@@ -27,12 +84,15 @@ const SignUp = () => {
         <div>
           <label
            
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Surname
           </label>
           <input 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
+          onChange={(e) =>
+            setFormData({ ...formData, surname: e.target.value })
+          }
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm
             rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
             p-2.5 dark:bg-gray-600 dark:border-gray-500
             dark:placeholder-gray-400 dark:text-white" required>
@@ -40,30 +100,26 @@ const SignUp = () => {
             
           </input>
         </div>
+        <p className="text-sm font-medium text-gray-900 dark:text-white" >
+              Country
+            </p>
+            <Select
+              options={options}
+              value={countrySelect}
+              onChange={changeHandler}
+            ></Select>
+        
         <div>
           <label
-           
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Country
-          </label>
-          <input 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
-            rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
-            p-2.5 dark:bg-gray-600 dark:border-gray-500
-            dark:placeholder-gray-400 dark:text-white" required>
-            
-            
-          </input>
-        </div>
-        <div>
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Email
           </label>
-          <input type="email" name="email" id="email" class="bg-gray-50 border
+          <input 
+          onChange={(e) =>
+            setFormData({ ...formData, email: e.target.value })
+          }type="email" name="email" id="email" className="bg-gray-50 border
             border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
             focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600
             dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
@@ -74,13 +130,16 @@ const SignUp = () => {
         </div>
         <div>
           <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Password
           </label>
-          <input type="password" name="password" id="password" placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
+          <input 
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }type="password" name="password"  placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm
             rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
             p-2.5 dark:bg-gray-600 dark:border-gray-500
             dark:placeholder-gray-400 dark:text-white" required>
@@ -91,13 +150,13 @@ const SignUp = () => {
 
         <div>
           <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Password Again
           </label>
           <input type="password" name="password" id="password" placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm
             rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
             p-2.5 dark:bg-gray-600 dark:border-gray-500
             dark:placeholder-gray-400 dark:text-white" required>
@@ -105,23 +164,54 @@ const SignUp = () => {
             
           </input>
         </div>
-        <div class="flex items-start">
-          <div class="flex items-start">
-            <div class="flex items-center h-1">
+        <div className="flex items-start">
+          <div className="flex items-start">
+            <div className="flex items-center h-1">
               
             </div>
             
           </div>
           
         </div>
-        <button
+        <button  disabled={disabled}
+          
           type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
+          className="
+          disabled:opacity-70 disabled:cursor-not-allowed w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+         
+       >
           Create an account
         </button>
         
       </form>
+      <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              marginTop: "10px",
+            }}
+          >
+            <p
+              style={{
+                color: "red",
+                display: formData.password.length >= 8 && "none",
+              }}
+            >
+              # Your password must be at least 8 characters.
+            </p>
+        
+        
+            <p
+              style={{
+                color: "red",
+                display:
+                  formData.password === formData.correctionPassword && "none",
+              }}
+            >
+              # Your password must be same as your password correction.
+            </p>
+          </div>
     </div>
 
     </div>
