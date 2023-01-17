d048577e284b47d225052cecd3c192096c7f50b0
hakan pesener <pesener@windowslive.com>
2023-01-17 19:06:06 +0300

    set other parts cause of deleted signup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,75 +1,54 @@
 import React from "react";
-import {RiUserFill, RiUserAddFill} from 'react-icons/ri'
-import {RiAdminLine} from 'react-icons/ri'
-import{RiLogoutCircleLine} from 'react-icons/ri'
-import { useEffect,useState } from "react";
-import { useLocation,useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
+import { RiAdminLine } from "react-icons/ri";
+import { RiLogoutCircleLine } from "react-icons/ri";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
+const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [user, setUser] = useState();
 
-
-
-
-const Navbar  = ( )=>{
-
-const dispatch = useDispatch()    
-const navigate= useNavigate()
-const location = useLocation()    
-const[user,setUser] = useState()
-
-const exit= async (id)=>{
-    await dispatch(logout(id))
-    setUser(null)
-    navigate('/')
-}
-
-
-useEffect(()=>{
-    if(localStorage.getItem('user') && !user){
-        setUser(JSON.parse(localStorage.getItem('user')))
+  useEffect(() => {
+    if (localStorage.getItem("user") && !user) {
+      setUser(JSON.parse(localStorage.getItem("user")));
     }
-},[location, user])
+  }, [location, user]);
 
-
-   
-    return (
-        <div className="bg-black">
-        <div className="container mx-auto h-20 flex items-center justify-between ">
-        
-        
+  return (
+    <div className="bg-black">
+      <div className="container mx-auto h-20 flex items-center justify-between ">
         <a className="text-white  text-3xl font-bold pl-10" href="/">
-        
-            NewBlog
-        
+          NewBlog
         </a>
 
-        { user? ( 
-<nav className=" flex gap-x-8 font-semibold text-opacity-90 text-white  pr-10  ">
-<a className="flex items-center gap-x-1 bg-blue-700 hover:bg-blue-800 rounded-full px-2 py-1 " href="/create">
-        <RiAdminLine size={20}/>
-             Admin Panel
-        </a>
-        <a className="flex items-center gap-x-1 bg-red-800 hover:bg-red-900 rounded-full px-4 py-1"   onClick={(e) =>{
-            exit(user.user._id)
-        }}>
-            <RiLogoutCircleLine size={20}/>
-             Sign Out
-        </a>
-
-      
-
-</nav>
-        ): ""
-    }
-        
-        
-        
-        </div>
-        
-        
-        </div>
-
-    )
-}
-export default Navbar
+        {user ? (
+          <nav className=" flex gap-x-8 font-semibold text-opacity-90 text-white  pr-10  ">
+            <a
+              className="flex items-center gap-x-1 bg-blue-700 hover:bg-blue-800 rounded-full px-2 py-1 "
+              href="/create"
+            >
+              <RiAdminLine size={20} />
+              Admin Panel
+            </a>
+            <a
+              className="flex items-center gap-x-1 bg-red-800 hover:bg-red-900 rounded-full px-4 py-1"
+              onClick={(e) => {
+                localStorage.removeItem("user");
+                setUser(null);
+                navigate("/signin");
+              }}
+            >
+              <RiLogoutCircleLine size={20} />
+              Sign Out
+            </a>
+          </nav>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+};
+export default Navbar;
